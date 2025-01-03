9d013f68ae47dcf2ff382148d4c16039d54adc0b
unknownorcuscancer <unknownorcuscancer@users.noreply.github.com>
2025-01-03 14:19:01 +0000

    ✅ Ready to clone and code.

--- myproject/cli.py
+++ myproject/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for myproject project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m myproject` and `$ myproject `.
 
     This is your program's entry point.
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for myproject package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("myproject", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="myproject",
+    version=read("myproject", "VERSION"),
+    description="Awesome myproject created by unknownorcuscancer",
+    url="https://github.com/unknownorcuscancer/myproject/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="unknownorcuscancer",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["myproject = myproject.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
